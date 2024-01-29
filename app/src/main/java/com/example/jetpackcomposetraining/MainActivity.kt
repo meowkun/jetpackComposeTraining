051d07d3d135ac7b23854350f405921f18e42839
Kecheng Tao <tkc1364411045@gmail.com>
2024-01-28 20:09:07 -0500

    Create profile details screen

--- app/src/main/java/com/example/jetpackcomposetraining/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetraining/MainActivity.kt
@@ -7,7 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.jetpackcomposetraining.profile_card.ProfileCardMainScreen
+import com.example.jetpackcomposetraining.profile_details.ProfileDetailsScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +29,24 @@ fun MainScreen() {
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
-        ProfileCardMainScreen()
+        UsersApplication()
+    }
+}
+
+@Composable
+fun UsersApplication() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "user_list") {
+        composable("user_list") {
+            ProfileCardMainScreen(navController)
+        }
+        composable("user_details/{userId}",
+            arguments = listOf(navArgument("userId") {
+                type = NavType.IntType
+            })
+        ) { navBackStackEntry ->
+            ProfileDetailsScreen(userId = navBackStackEntry.arguments?.getInt("userId") ?: 0,
+                navController = navController)
+        }
     }
 }
